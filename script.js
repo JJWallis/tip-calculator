28ce312d358dc15f833f4a36eb6cd50d2e21138d
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-07-20 10:10:52 +0100

    Calculate func in progress

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// If passes - creates dynamic property name that matches e.target.id (store on var - use for label ref too) via interpolation 
-// Runs math func (checks whether Obj.keys(objName).length === 3) 
-
-
 // Btns Click event: 
 // e.preventDefault() - submit type by default (or type="button")
 // Removes % sign (spread operator or split())
 // Assigns value to input-tip property in obj (either creating or re-assigning)
-// Runs math func that checks whether Obj.keys(objName).length === 3 
+// Runs math func that checks whether Obj.keys(objName).length === 3 or any values !== 0 
 
 // Math formulas - using obj prop names: 
 // tipPerPerson = bill / peopleNum * 0.10 (10% - 10/100) = 2.4 | updates tip-total el 
@@ -25,17 +21,28 @@ const values = {}
 function validate (x) {
     const num = +x.value 
     const classes = x.classList 
-    const label = x.matches('#input-tip') ? x.parentElement.previousElementSibling : x.previousElementSibling
+    const label = x.matches('#input-tip') ? x.parentElement.previousElementSibling : x.previousElementSibling  
 
-    if (Number.isNaN(num) || num === 0) { // Only happens if not present already? 
-        const errorMsg = document.createElement('p')
-        errorMsg.innerText = 'Please enter a valid number above zero'
-        errorMsg.classList.add('error-msg')
-        label.append(errorMsg)
-        classes.add('error')
+    if (Number.isNaN(num) || num === 0) { 
+        if (label.children.length === 0) {
+            const errorMsg = document.createElement('p')
+            errorMsg.innerText = 'Please enter a valid number above zero'
+            errorMsg.classList.add('error-msg')
+            label.append(errorMsg)
+            classes.add('error')
+        }
     } else {
         if (classes.contains('error')) classes.remove('error')
-        if (label.children.length > 0) label.children[0].remove()
+        if (label.children.length > 0) label.children[0].remove() 
+        values[x.id] = num 
+        calculate()
+    }
+}
+
+function calculate () {
+    const arr = Object.keys(values)
+    if (arr.length === 3) {
+
     }
 }
 
