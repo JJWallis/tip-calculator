e80331b09c3cdf77b7e3c219139f1f27fa27ec3c
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-07-20 12:46:46 +0100

    Working - 2 bugs in notes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Btns Click event: 
-// e.preventDefault() - submit type by default (or type="button")
-// Removes % sign (spread operator or split())
-// Assigns value to input-tip property in obj (either creating or re-assigning)
-// Runs math func that checks whether Obj.keys(objName).length === 3 or any values !== 0 
+// Custom input in disabled state if tip-btn in a selected state + vice-versa (visually showcase via disabled class + ternary operator) 
+// No error mssg to display if btn selected (on custom input) 
+// Better method of extrapolating % 
+// Refactor 
 
 const form = document.querySelector('#calc-form')
+const btnsContainer = document.querySelector('#btns-container')
 const billInput = document.querySelector('#input-bill')
 const tipInput = document.querySelector('#input-tip')
 const pplInput = document.querySelector('#input-ppl')
@@ -13,7 +13,7 @@ const total = document.querySelector('#total')
 const btnReset = document.querySelector('#btn-reset')
 const values = {}
 
-function validate (x) {
+function inputValidate (x) {
     const num = +x.value 
     const classes = x.classList 
     const label = x.matches('#input-tip') ? x.parentElement.previousElementSibling : x.previousElementSibling  
@@ -34,6 +34,13 @@ function validate (x) {
     }
 }
 
+function btnValidate (val) { 
+    const arr = [...val]
+    arr.pop()
+    const num = arr.join('')
+    return +num
+}
+
 function calculate () {
     const arr = Object.keys(values)
     if (arr.length === 3) {
@@ -42,11 +49,32 @@ function calculate () {
         const totalPerPerson = () => values['input-bill'] / values['input-ppl'] + tip
         tipTotal.innerText = tip
         total.innerText = totalPerPerson()
+        btnReset.classList.toggle('selected')
+
     }
 }
 
 /////////////// EVENT LISTENERS //////////////////////
 
 form.addEventListener('change', e => {
-    validate(e.target)
+    inputValidate(e.target)
+})
+
+btnsContainer.addEventListener('click', e => {
+    const target = e.target
+    if (target.matches('button')) {
+        e.preventDefault()
+        const num = btnValidate(target.innerText)
+        const btns = document.querySelectorAll('.tip-btn') // Refactor in global scope as in next event listener too? 
+        btns.forEach(el => el.classList.remove('selected')) // or sep func 
+        target.classList.toggle('selected')
+        values['input-tip'] = num
+        calculate()
+    }
+})
+
+btnReset.addEventListener('click', e => {
+    const btns = document.querySelectorAll('.tip-btn')
+    btns.forEach(el => el.classList.remove('selected'))
+    btnReset.classList.remove('selected')
 })
